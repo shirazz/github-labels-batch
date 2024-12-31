1fb2670d40fadd4144b719bab5cea6d34b158807
Shiraz Samad <shirazwaits@gmail.com>
2025-01-01 00:26:47 +0400

    remove unused method

--- index.ts
+++ index.ts
@@ -22,17 +22,6 @@ async function addLabels(name: string, description: string, color: string) {
 	});
 }
 
-async function removeLabels(name: string) {
-	await octokit.request("DELETE /repos/{owner}/{repo}/labels/{name}", {
-		owner: Bun.env.GH_OWNER || "",
-		repo: Bun.env.GH_REPO || "",
-		name,
-		headers: {
-			"X-GitHub-Api-Version": Bun.env.X_GITHUB_API_VERSOIN,
-		},
-	});
-}
-
 // * loop through labels and create them one by one
 for (const label of labels) {
 	await addLabels(label.name, label.description, label.color)
